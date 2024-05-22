7c9f9b00adb994e01794537382214c894662c52a
Sarvar Abdullajonov <152815541+sarvar0911@users.noreply.github.com>
2024-05-22 22:40:47 +0500

    review fixed

--- main/permissions.py
+++ main/permissions.py
@@ -4,7 +4,6 @@ from rest_framework import permissions
 class IsReviewer(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        # Allow all users to view reviews
         if request.method in permissions.SAFE_METHODS:
             return True
         return request.user and request.user.is_authenticated and request.user.is_reviewer
